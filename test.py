ef2479baf3af7cbb85c358384ec76aac7e70f7a0
Naryna Azizpour <narynaa@uchicago.edu>
2026-05-08 15:42:11 -0500

    initial push

---
+++ test.py
@@ -0,0 +1,23 @@
+from calculator import Calculator
+
+calc = Calculator()
+
+
+def test_add():
+    assert calc.add(2, 3) == 5
+
+
+def test_subtract():
+    assert calc.subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert calc.multiply(4, 3) == 12
+
+
+def test_divide():
+    assert calc.divide(10, 2) == 5
+
+
+def test_divide_by_zero():
+    assert calc.divide(10, 0) == "Cannot divide by zero"
